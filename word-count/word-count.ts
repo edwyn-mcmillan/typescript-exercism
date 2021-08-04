4efaebb52f519b0c8fab5c29b60e492f5984bd85
Edwyn McMillan <codemcmillan@gmail.com>
2021-08-04 21:15:12 +1200

    regex selection

--- word-count/word-count.ts
+++ word-count/word-count.ts
@@ -1,11 +1,19 @@
 export default class Words {
-  public wordCount(words: string) {
-    let returnObj: string,
-      {} = {};
-    let wordArr: string[] = words.split(" ");
+  public count(words: string): Map<string, number> {
+    const wordCount = new Map();
 
-    wordArr.map((word) => {
-      returnObj[word] = word;
-    });
+    /* ---Regex Notes ---
+
+    () grouping words.
+    [^  ] negated set. Match any character that is not in the set
+    /s targets spaces.
+    g is global flag.
+
+    */
+
+    const regex = /([^\s])+/g;
+    const words = words.toLowerCase().match(regex);
+
+    return wordCount;
   }
 }

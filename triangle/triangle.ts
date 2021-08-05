e56bd2cf1929b54a2ce191b84616e3fb4fb7ff53
Edwyn McMillan <codemcmillan@gmail.com>
2021-08-05 13:45:54 +1200

    triangle passes tests

--- triangle/triangle.ts
+++ triangle/triangle.ts
@@ -1,11 +1,35 @@
 export default class Triangle {
-  sides: number[]
+  sides: number[];
 
   constructor(...sides: number[]) {
-    this.sides = sides
+    /*
+    .sort compares 2 values 
+    if the result is negative, a is sorted before b
+    if the result is positive, b is sorted before a
+    if the result is 0 there are no order changes
+  */
+
+    this.sides = sides.sort((a, b) => a - b);
   }
 
-  kind() {
-    return ''
+  kind(): string {
+    const [a, b, c] = this.sides;
+
+    /*
+    equilateral = all sides are the same length
+    isosceles = atleast two sides are the same length
+    scalene = all sides are different lengths
+  */
+
+    switch (true) {
+      case a <= 0 || a + b <= c: //defining a triangle
+        throw new Error("Not a triangle");
+      case a === c:
+        return "equilateral";
+      case a === b || b === c:
+        return "isosceles";
+      default:
+        return "scalene";
+    }
   }
 }

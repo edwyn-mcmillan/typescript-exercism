806c324e2dc11a4add3543776a1af54dba51575c
Edwyn McMillan <codemcmillan@gmail.com>
2021-08-08 11:14:25 +1200

    refactor, dynamic onPlanet methods

--- space-age/space-age.ts
+++ space-age/space-age.ts
@@ -1,30 +1,31 @@
-enum Planet {
-  Earth = 31557600,
-  Mercury = 0.2408467,
-  Venus = 0.61519726,
-  Mars = 1.8808158,
-  Jupiter = 11.862615, 
-  Saturn = 29.447498,
-  Uranus = 84.016846,
-  Neptune = 164.79132
+const planets: { [key: string]: number } = {
+  Earth: 1.0,
+  Mercury: 0.2408467,
+  Venus: 0.61519726,
+  Mars: 1.8808158,
+  Jupiter: 11.862615,
+  Saturn: 29.447498,
+  Uranus: 84.016846,
+  Neptune: 164.79132,
+};
+
+const EARTH_YEAR_SECONDS = 31_557_600;
+
+interface SpaceAge {
+  [key: string]: any;
 }
 
-export default class SpaceAge {
-  seconds: number;
-  constructor(seconds: number) {
-    this.seconds = seconds;
+class SpaceAge {
+  constructor(public readonly seconds: number) {
+    for (const planet in planets) {
+      this[`on${planet}`] = (): number =>
+        this.orbitalPeriodInEarthYears(planets[planet]);
+    }
   }
 
   private orbitalPeriodInEarthYears(period: number): number {
-    return Number(((this.seconds / Planet.Earth) / period).toFixed(2));
+    return Math.round((this.seconds / EARTH_YEAR_SECONDS / period) * 100) / 100;
   }
-
-  onEarth = () => this.orbitalPeriodInEarthYears(1);
-  onMercury = () => this.orbitalPeriodInEarthYears(Planet.Mercury);
-  onVenus = () => this.orbitalPeriodInEarthYears(Planet.Venus);
-  onMars = () => this.orbitalPeriodInEarthYears(Planet.Mars);
-  onJupiter = () => this.orbitalPeriodInEarthYears(Planet.Jupiter);
-  onSaturn = () => this.orbitalPeriodInEarthYears(Planet.Saturn);
-  onUranus = () => this.orbitalPeriodInEarthYears(Planet.Uranus);
-  onNeptune = () => this.orbitalPeriodInEarthYears(Planet.Neptune);
 }
+
+export default SpaceAge;

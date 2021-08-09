1ae459ea8bb94d2c24a492b8a21f089e3047a37e
Edwyn McMillan <codemcmillan@gmail.com>
2021-08-09 18:05:15 +1200

    isograms passes some tests

--- isogram/isogram.ts
+++ isogram/isogram.ts
@@ -1,7 +1,18 @@
 class Isogram {
-  static isIsogram(/* Parameters go here */) {
-    // Your code here
+  static isIsogram(input: string): boolean {
+    const letters: string[] = [];
+    const words = input
+      .toLowerCase()
+      .replace(/[^a-z]/, "")
+      .split("");
+    words.map((char) => {
+      if (letters.includes(char)) {
+        return false;
+      }
+      letters.push(char);
+    });
+    return true;
   }
 }
 
-export default Isogram
+export default Isogram;

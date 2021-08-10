f0608a48a62501774605095aa45dafe2c0d15420
Edwyn McMillan <codemcmillan@gmail.com>
2021-08-11 10:58:55 +1200

    anagram passes all tests

--- anagram/anagram.ts
+++ anagram/anagram.ts
@@ -1,4 +1,28 @@
 export default class Anagram {
+  private word: string;
 
-    
+  constructor(input: string) {
+    this.word = input.toLowerCase();
+  }
+
+  matches(...wordList: string[]): string[] {
+    const result: string[] = [];
+    wordList.forEach((word) => {
+      this.isAnagram(word) ? result.push(word) : null;
+    });
+    return result;
+  }
+
+  private isAnagram(wordFromList: string) {
+    if (this.isSameWord(wordFromList)) return false;
+    return this.sortedLetters(this.word) === this.sortedLetters(wordFromList);
+  }
+
+  private isSameWord(word: string): boolean {
+    return word.toLowerCase() === this.word;
+  }
+
+  private sortedLetters(word: string): string {
+    return word.toLowerCase().split("").sort().join("");
+  }
 }

469fec8a19551bf16d1e3d7a4d83bbc440648d47
Edwyn McMillan <codemcmillan@gmail.com>
2021-08-11 10:26:32 +1200

    acronym passes all tests

--- acronym/acronym.ts
+++ acronym/acronym.ts
@@ -1,5 +1,11 @@
 export default class Acronym {
   public static parse(phrase: string): string {
-    return "";
+    /* ---- Regex ----
+      ?= is a positive look up, checks to see if it's there but doesn't match.
+      regex = Capital letter followed by lowercase or first character of a word.
+    */
+    const regex = /[A-Z](?=[a-z]+)|\b\w/g;
+    const acronym = phrase.match(regex);
+    return acronym === null ? "" : acronym.join("").toUpperCase();
   }
 }

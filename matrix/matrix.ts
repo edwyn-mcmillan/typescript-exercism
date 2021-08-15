cfc8f40fa25007662668739dcc46d927166c849e
Edwyn McMillan <codemcmillan@gmail.com>
2021-08-15 14:46:31 +1200

    matrix passes tests

--- matrix/matrix.ts
+++ matrix/matrix.ts
@@ -1,7 +1,20 @@
 class Matrix {
-  constructor(/* Parameters go here */) {
-    // Your code here
+  public rows: number[][];
+  public columns: number[][];
+
+  constructor(input: string) {
+    this.rows = input
+      .split("\n")
+      .map((row) => row.split(" ").map((value) => Number(value)));
+
+    this.columns = [];
+
+    for (let i = 0; i < this.rows.length; i++) {
+      this.columns.push(this.rows.map(row => row[i]))
+    }
   }
 }
 
-export default Matrix
+export default Matrix;
+
+

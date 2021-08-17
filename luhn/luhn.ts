c37a01b95160fc5a401a3808aa3c660e41ece816
Edwyn McMillan <codemcmillan@gmail.com>
2021-08-17 14:52:15 +1200

    luhn passes tests

--- luhn/luhn.ts
+++ luhn/luhn.ts
@@ -13,23 +13,15 @@ class Luhn {
     }
 
     let total = 0;
-    console.log(cleanInput);
-
     for (let i = 0; i < cleanInput.length; i++) {
       i % 2 === 0 ? cleanInput[i] * 2 : cleanInput[i]
       cleanInput[i] > 9 ? cleanInput[i] - 9: cleanInput[i]
       total += cleanInput[i];
     }
-
     return total % 10 == 0;
   }
 }
 
 export default Luhn;
 
-// if (i % 2 === 0) {
-//     cleanInput[i] = cleanInput[i] * 2;
-//     if (cleanInput[i] > 9) {
-//       cleanInput[i] = cleanInput[i] - 9;
-//     }
-//   }
+

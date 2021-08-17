cdcef75bc432caae7a071ee00066b2599cf1edea
Edwyn McMillan <codemcmillan@gmail.com>
2021-08-17 14:26:58 +1200

    luhn passes tests

---
+++ luhn/luhn.ts
@@ -0,0 +1,35 @@
+class Luhn {
+  static valid(input: string): boolean {
+    const cleanInput: number[] = input
+      .split(" ")
+      .join("")
+      .split("")
+      .reverse()
+      .map((digit) => Number(digit));
+
+    // Check for invalid input.
+    if (cleanInput.length <= 1 || cleanInput.includes(NaN)) {
+      return false;
+    }
+
+    let total = 0;
+    console.log(cleanInput);
+
+    for (let i = 0; i < cleanInput.length; i++) {
+      i % 2 === 0 ? cleanInput[i] * 2 : cleanInput[i]
+      cleanInput[i] > 9 ? cleanInput[i] - 9: cleanInput[i]
+      total += cleanInput[i];
+    }
+
+    return total % 10 == 0;
+  }
+}
+
+export default Luhn;
+
+// if (i % 2 === 0) {
+//     cleanInput[i] = cleanInput[i] * 2;
+//     if (cleanInput[i] > 9) {
+//       cleanInput[i] = cleanInput[i] - 9;
+//     }
+//   }

b9cb760dc4a7117a3f12ddfd8f772d8ab5e94d6e
Edwyn McMillan <codemcmillan@gmail.com>
2021-08-18 13:29:31 +1200

    collatz-conjecture refactor

--- collatz-conjecture/collatz-conjecture.ts
+++ collatz-conjecture/collatz-conjecture.ts
@@ -1,25 +1,15 @@
 class CollatzConjecture {
   static steps(input: number): number {
-    let n = input;
-    let stepCount = 0;
-
-    if (n <= 0) {
+    if (input <= 0) {
       throw new Error("Only positive numbers are allowed");
     }
 
-    while (n) {
-      switch (true) {
-        case n === 1: //endpoint
-          return stepCount;
-        case n % 2 === 0: //n is even
-          stepCount++;
-          n = n / 2;
-          break;
-        default: //n is odd
-          stepCount++;
-          n = n * 3 + 1;
-          break;
-      }
+    let n = input;
+    let stepCount = 0;
+
+    while (n !== 1) {
+      stepCount += 1;
+      n = n % 2 ? n * 3 + 1 : n / 2;
     }
     return stepCount;
   }

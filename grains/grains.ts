abf7e05bbaa916b5eaf8d0aefb112d2389e01bd6
Edwyn McMillan <codemcmillan@gmail.com>
2021-08-20 11:35:52 +1200

    grains passes all tests

--- grains/grains.ts
+++ grains/grains.ts
@@ -1,9 +1,17 @@
 export default class Grains {
-  static square(input: number): number {
-    return 0;
+  static square(square: number): number {
+    if (square <= 0 || square > 64) {
+      throw Error;
+    }
+
+    return Math.pow(2, square - 1);
   }
 
   static total(): number {
-    return 0;
+    let sum = 0;
+    for (let i = 0; i <= 64; i++) {
+      sum += Math.pow(2, i - 1);
+    }
+    return sum;
   }
 }
